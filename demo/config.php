deb560441b4e3b951be0ea14df139ea1e58049a9
deseven <de7@deseven.info>
2014-07-15 05:12:26 +0600

    first revision

---
+++ demo/config.php
@@ -0,0 +1,51 @@
+<?php
+
+$params["system"] = array(
+	"root" => realpath(dirname(__FILE__)),
+	"clean_urls" => false, // not really working right now, don't use
+	"404_page" => "404", // a page which we will use when the page is not found
+	"main_page" => "main_page", // default page, you can remove to pick random page every time
+	"show_errors" => true
+);
+
+$params["menus"] = array(
+	"html" => '<li><a href="{VODKA:URL}" id="{VODKA:NAME}" class="{VODKA:CLASS}">{VODKA:TITLE}</a></li>', // html of the menu element
+	"selected_class" => "selected",
+	"selected_class_first" => "selected_top",  // here we can define that we need to add another class for the first
+	"selected_class_last" => "selected_bottom" // and the last selected element
+);
+
+$params["aliases"] = array(
+	"other_page" => "third", // there we can define various aliases, in this case, url with ?other_page will lead us to the page called "third"
+	"i=like&to=use&some=variables" => "second"
+);
+
+$params["pages"] = array(
+	array(
+		"path" => "pages/main.html", // location (relative to the root dir)
+		"name" => "main",            // internal page name
+		"title" => "main page"       // title for the menu entry
+	),
+	array(
+		"path" => "pages/second.html",
+		"name" => "second",
+		"title" => "second page"
+	),
+	array(
+		"path" => "pages/third.html",
+		"name" => "third",
+		"title" => "third page"
+	),
+	array(
+		"path" => "pages/404.html",
+		"name" => "404",
+		"title" => "404",
+		"visible" => false // menu visibility flag
+	)
+);
+
+$params["templates"] = array(
+	"demo" => "tpl/demo" // name and path to the template dir
+);
+
+?>
---
+++ demo/index.php
@@ -0,0 +1,38 @@
+<?php
+
+require "../vodka.class.php"; // main class
+require "config.php"; // this is where all our settings are
+
+$vodka = new vodka($params); // init
+
+$vodka->loadTemplate("demo"); // loading template
+
+$vodka->loadCurrentPage(); // this will load the current page, you can also use loadPage($page) instead
+$vodka->buildCurrentMenu(); // this will build the menu for the current page, once again, you can use buildMenu($page) instead
+
+// let's insert some lorem ipsum
+$lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+$vodka->replaceVar("{LOREM}",$lorem);
+
+// let's insert some more info to the head section
+$vodka->appendHead('<meta name="GENERATOR" content="Vodka">');
+
+// let's insert something based on the current page
+$page = $vodka->getCurrentPage();
+switch ($page["name"]) {
+	case "main":
+		$vodka->replaceVar("{GREETING}","Hello, this is Vodka speaking!");
+		break;
+	case "second":
+		$vodka->replaceVar("{CLASS}","second");
+		break;
+	case "third":
+		$vodka->replaceVar("{CLASS}","third");
+		break;
+	case "404":
+		$vodka->replaceVar("{WARN}","You shouldn't be there.");
+}
+
+$vodka->buildCurrentPage(); // finally, we are ready to output our page
+
+?>
