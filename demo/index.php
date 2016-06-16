bbd712b19b735c16d277382d3e4e59da4a3ebc3e
deseven <de7@deseven.info>
2016-06-16 12:40:07 +0200

    updated demo

--- demo/index.php
+++ demo/index.php
@@ -33,8 +33,12 @@ switch ($page["name"]) {
 		$vodka->replaceVar("{WARN}","You shouldn't be there.");
 		break;
 	case "usage":
-		$vodka->appendHead('<script src="https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js"></script>');
+		$vodka->appendHead('<script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>');
 }
+
+// let's insert generation info
+$generation_time = round(microtime(true) - $vodka->getStartTime(),4);
+$vodka->replaceVar("{FOOTER_INFO}",'generated with <a href="https://github.com/deseven/vodka">Vodka</a> rev.'.$vodka::rev." in $generation_time sec");
 
 $vodka->buildCurrentPage(); // finally, we are ready to output our page
 
